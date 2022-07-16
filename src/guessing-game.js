9fd7960772809ab1961a2e46bde2ba7e309b5aa5
dimaPolonez <dima-barxat@yandex.ru>
2022-07-16 14:17:27 +0300

    feat: task complete

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.minNum = min;
+        this.maxNum = max;
     }
 
     guess() {
-
+        this.result = Math.ceil(this.minNum + (this.maxNum - this.minNum) / 2);
+        return this.result;
     }
 
     lower() {
-
+        this.maxNum = this.result;
     }
 
     greater() {
-
+        this.minNum = this.result;
     }
 }
 
